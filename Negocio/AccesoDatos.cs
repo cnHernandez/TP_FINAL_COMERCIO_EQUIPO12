462f35af86ed27bda27af02124131a1d39fd582e
nicolas hernandez <101599191+nicccooh@users.noreply.github.com>
2023-12-18 18:22:40 -0300

    Acceso a Datos

---
+++ Negocio/AccesoDatos.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Negocio
+{
+    using System;
+    using System.Data;
+    using System.Data.SqlClient;
+
+    public class AccesoDatos : IDisposable
+    {
+        public SqlConnection Conexion;
+        public SqlCommand Comando;
+        public SqlDataReader Lector;
+
+        public SqlDataReader lector
+        {
+            get { return Lector; }
+        }
+
+        public AccesoDatos()
+        {
+
+            Conexion = new SqlConnection("server=; database=; integrated security=true");
+            Comando = new SqlCommand();
+        }
+
+        public void setearParametros(string nombre, object valor)
+        {
+            Comando.Parameters.AddWithValue(nombre, valor);
+        }
+
+        public void SetearProcedimiento(string sp)
+        {
+            Comando.CommandType = System.Data.CommandType.StoredProcedure;
+            Comando.CommandText = sp;
+        }
+
+        public void SetearQuery(string Query)
+        {
+            Comando.CommandType = System.Data.CommandType.Text;
+            Comando.CommandText = Query;
+        }
+
+        public void CerrarConexion()
+        {
+            if (Lector != null)
+            {
+                Lector.Close();
+            }
+            if (Conexion != null && Conexion.State != ConnectionState.Closed)
+            {
+                Conexion.Close();
+            }
+        }
+
+        public void ejecutarAccion()
+        {
+            Comando.Connection = Conexion;
+            try
+            {
+                Conexion.Open();
+                Comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                CerrarConexion();
+            }
+        }
+
+        public object ejecutarScalar()
+        {
+            Comando.Connection = Conexion;
+            try
+            {
+                Conexion.Open();
+                return Comando.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                CerrarConexion();
+            }
+        }
+       
+
+        public void Dispose()
+        {
+            // Implementa la interfaz IDisposable para cerrar la conexión y liberar recursos
+            CerrarConexion();
+        }
+
+        
+        public void EjecutarLectura()
+        {
+            Comando.Connection = Conexion;
+            try
+            {
+                Conexion.Open();
+                Lector = Comando.ExecuteReader();
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+            }
+        }
+    }
+}
